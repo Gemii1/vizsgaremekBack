ff1d4878a4fc2ac217a4743eae49ec2e67157a95
fidrer1 <nagykristof215@gmail.com>
2025-01-31 14:45:11 +0100

    Pom.xml 3.3.5 3.4.2ből

--- src/main/java/hu/fitness/controller/ClientController.java
+++ src/main/java/hu/fitness/controller/ClientController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/client")
 @Tag(name = "Client Test Functions", description = "Manage Clients")
