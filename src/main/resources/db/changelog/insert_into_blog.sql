5dab2cbc76f49220d31e7662dc9ec7f163e58cf0
fidrer1 <nagykristof215@gmail.com>
2025-04-09 09:06:07 +0200

    Képek inicializasa -Gemi

--- src/main/resources/db/changelog/insert_into_blog.sql
+++ src/main/resources/db/changelog/insert_into_blog.sql
@@ -1,2 +1,2 @@
-insert into blog (blog_type, title, header_text, main_text, file_id, trainer_id)
-values ('TRAINING', 'title1', 'HeaderText', 'MainText', 1, 101);
+insert into blog (id, blog_type, title, header_text, main_text, file_id, trainer_id)
+values (1,'TRAINING', 'title1', 'HeaderText', 'MainText', null, 101);
--- src/main/resources/db/changelog/insert_into_trainer.sql
+++ src/main/resources/db/changelog/insert_into_trainer.sql
@@ -1,3 +1,3 @@
 insert into trainer (id, name, birth_date, gender, qualification, phone_number, rating, login_id, file_id) values
-    (101,'TrainerTeszt1','2002-09-04','MALE','PERSONAL_TRAINER', '36201234567',4,4,311),
-    (102, 'TrainerTeszt2', '1983-03-17', 'MALE', 'FITNESS_INSTRUCTOR', '36301234576', 4,5,312);
+    (101,'TrainerTeszt1','2002-09-04','MALE','PERSONAL_TRAINER', '36201234567',4,4,null),
+    (102, 'TrainerTeszt2', '1983-03-17', 'MALE', 'FITNESS_INSTRUCTOR', '36301234576', 4,5,null);
